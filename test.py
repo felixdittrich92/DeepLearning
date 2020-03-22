120ca5ecfd0b418ea069647540411f6296f7f021
Felix <felixdittrich92@gmail.com>
2020-03-22 18:37:56 +0100

    TF GPU runs

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
+import tensorflow as tf
+
+value = tf.test.is_gpu_available()
+print(value)
+
+print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+
 print("hello")
 print("hello Ubuntu")
