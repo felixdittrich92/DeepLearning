4376572fc8ab05cd6225b4092541fcebb5b51f89
Felix <felixdittrich92@gmail.com>
2020-08-18 16:43:28 +0200

    func DCGAN

---
+++ 7_DeepLearning-GANs/Zusatz_funktionaleAPI_DCGAN/DCGAN.py
@@ -0,0 +1,57 @@
+# https://github.com/osh/KerasGAN/blob/master/MNIST_CNN_GAN_v2.ipynb
+
+# Build Generative model ...
+nch = 200
+g_input = Input(shape=[100])
+H = Dense(nch*14*14, init='glorot_normal')(g_input)
+H = BatchNormalization(mode=2)(H)
+H = Activation('relu')(H)
+H = Reshape( [nch, 14, 14] )(H)
+H = UpSampling2D(size=(2, 2))(H)
+H = Convolution2D(nch/2, 3, 3, border_mode='same', init='glorot_uniform')(H)
+H = BatchNormalization(mode=2)(H)
+H = Activation('relu')(H)
+H = Convolution2D(nch/4, 3, 3, border_mode='same', init='glorot_uniform')(H)
+H = BatchNormalization(mode=2)(H)
+H = Activation('relu')(H)
+H = Convolution2D(1, 1, 1, border_mode='same', init='glorot_uniform')(H)
+g_V = Activation('sigmoid')(H)
+generator = Model(g_input,g_V)
+generator.compile(loss='binary_crossentropy', optimizer=opt)
+generator.summary()
+
+
+# Build Discriminative model ...
+d_input = Input(shape=shp)
+H = Convolution2D(256, 5, 5, subsample=(2, 2), border_mode = 'same', activation='relu')(d_input)
+H = LeakyReLU(0.2)(H)
+H = Dropout(dropout_rate)(H)
+H = Convolution2D(512, 5, 5, subsample=(2, 2), border_mode = 'same', activation='relu')(H)
+H = LeakyReLU(0.2)(H)
+H = Dropout(dropout_rate)(H)
+H = Flatten()(H)
+H = Dense(256)(H)
+H = LeakyReLU(0.2)(H)
+H = Dropout(dropout_rate)(H)
+d_V = Dense(2,activation='softmax')(H)
+discriminator = Model(d_input,d_V)
+discriminator.compile(loss='categorical_crossentropy', optimizer=dopt)
+discriminator.summary()
+
+
+# Freeze weights in the discriminator for stacked training
+def make_trainable(net, val):
+    net.trainable = val
+    for l in net.layers:
+       l.trainable = val
+make_trainable(discriminator, False)
+
+# Build stacked GAN model
+gan_input = Input(shape=[100])
+H = generator(gan_input)
+gan_V = discriminator(H)
+GAN = Model(gan_input, gan_V)
+GAN.compile(loss='categorical_crossentropy', optimizer=opt)
+GAN.summary()
+
+
