fb2452f79f4f878c363049659ba5fe315bdf43ce
Felix <felixdittrich92@gmail.com>
2020-09-08 16:13:07 +0200

    ResNet

--- 8_DeepLearning-Autoencoder/CNN_Autoencoder.py
+++ 8_DeepLearning-Autoencoder/CNN_Autoencoder.py
@@ -7,11 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
-# fix CuDnn problem
+# Fix CuDnn problem
 gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
   try:
-    tf.config.experimental.set_virtual_device_configuration(gpus[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=4096)])
+    tf.config.experimental.set_memory_growth(gpus[0], True)
   except RuntimeError as e:
     print(e)
 
--- More-DL/ResidualNetwork_rebuild.py
+++ More-DL/ResidualNetwork_rebuild.py
@@ -1,106 +1,75 @@
+import numpy as np
 import tensorflow as tf
+
+# Fix CuDnn problem
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+  except RuntimeError as e:
+    print(e)
+
+from tensorflow.keras.models import Model
 from tensorflow.keras.layers import *
-from tensorflow.keras import Model
 
-# do : Refactor to func
+class ResNet34():
+    def __init__(self, input_shape, output_dim):
+        x = Input(shape=input_shape)
+        h = Conv2D(64, kernel_size=(7, 7), strides=(2, 2), padding='same')(x)
+        h = BatchNormalization()(h)
+        h = Activation('relu')(h)
+        h = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(h)
+        h = self._building_block(h, channel_out=64)
+        h = self._building_block(h, channel_out=64)
+        h = self._building_block(h, channel_out=64)
+        h = Conv2D(128, kernel_size=(1, 1), strides=(2, 2))(h)
+        h = self._building_block(h, channel_out=128)
+        h = self._building_block(h, channel_out=128)
+        h = self._building_block(h, channel_out=128)
+        h = self._building_block(h, channel_out=128)
+        h = Conv2D(256, kernel_size=(1, 1), strides=(2, 2))(h)
+        h = self._building_block(h, channel_out=256)
+        h = self._building_block(h, channel_out=256)
+        h = self._building_block(h, channel_out=256)
+        h = self._building_block(h, channel_out=256)
+        h = self._building_block(h, channel_out=256)
+        h = self._building_block(h, channel_out=256)
+        h = Conv2D(512, kernel_size=(1, 1), strides=(2, 2))(h)
+        h = self._building_block(h, channel_out=512)
+        h = self._building_block(h, channel_out=512)
+        h = self._building_block(h, channel_out=512)
+        h = GlobalAveragePooling2D()(h)
+        h = Dense(1000, activation='relu')(h)
+        y = Dense(output_dim, activation='softmax')(h)
+        self.model = Model(x, y)
 
-class ResBlock(Model):
-    def __init__(self, channels, stride=1):
-        super(ResBlock, self).__init__(name='ResBlock')
-        self.flag = (stride != 1)
-        self.conv1 = Conv2D(channels, 3, stride, padding='same')
-        self.bn1 = BatchNormalization()
-        self.conv2 = Conv2D(channels, 3, padding='same')
-        self.bn2 = BatchNormalization()
-        self.relu = ReLU()
-        if self.flag:
-            self.bn3 = BatchNormalization()
-            self.conv3 = Conv2D(channels, 1, stride)
+    def __call__(self):
+        return self.model
 
-    def call(self, x):
-        x1 = self.conv1(x)
-        x1 = self.bn1(x1)
-        x1 = self.relu(x1)
-        x1 = self.conv2(x1)
-        x1 = self.bn2(x1)
-        if self.flag:
-            x = self.conv3(x)
-            x = self.bn3(x)
-        x1 = Add()([x, x1])
-        x1 = self.relu(x1)
-        return x1
-
-
-class ResNet34(Model):
-    def __init__(self):
-        super(ResNet34, self).__init__(name='ResNet34')
-        self.conv1 = Conv2D(64, 7, 2, padding='same')
-        self.bn = BatchNormalization()
-        self.relu = ReLU()
-        self.mp1 = MaxPooling2D(3, 2)
-
-        self.conv2_1 = ResBlock(64)
-        self.conv2_2 = ResBlock(64)
-        self.conv2_3 = ResBlock(64)
-
-        self.conv3_1 = ResBlock(128, 2)
-        self.conv3_2 = ResBlock(128)
-        self.conv3_3 = ResBlock(128)
-        self.conv3_4 = ResBlock(128)
-
-        self.conv4_1 = ResBlock(256, 2)
-        self.conv4_2 = ResBlock(256)
-        self.conv4_3 = ResBlock(256)
-        self.conv4_4 = ResBlock(256)
-        self.conv4_5 = ResBlock(256)
-        self.conv4_6 = ResBlock(256)
-
-        self.conv5_1 = ResBlock(512, 2)
-        self.conv5_2 = ResBlock(512)
-        self.conv5_3 = ResBlock(512)
-
-        self.pool = GlobalAveragePooling2D()
-        self.fc1 = Dense(512, activation='relu')
-        self.dp1 = Dropout(0.5)
-        self.fc2 = Dense(512, activation='relu')
-        self.dp2 = Dropout(0.5)
-        self.fc3 = Dense(64)
-
-    def call(self, x):
-        x = self.conv1(x)
-        x = self.bn(x)
-        x = self.relu(x)
-        x = self.mp1(x)
-
-        x = self.conv2_1(x)
-        x = self.conv2_2(x)
-        x = self.conv2_3(x)
-
-        x = self.conv3_1(x)
-        x = self.conv3_2(x)
-        x = self.conv3_3(x)
-        x = self.conv3_4(x)
-
-        x = self.conv4_1(x)
-        x = self.conv4_2(x)
-        x = self.conv4_3(x)
-        x = self.conv4_4(x)
-        x = self.conv4_5(x)
-        x = self.conv4_6(x)
-
-        x = self.conv5_1(x)
-        x = self.conv5_2(x)
-        x = self.conv5_3(x)
-
-        x = self.pool(x)
-        x = self.fc1(x)
-        x = self.dp1(x)
-        x = self.fc2(x)
-        x = self.dp2(x)
-        x = self.fc3(x)
-        return x
+    def _building_block(self, x, channel_out=64):
+        h = Conv2D(channel_out, kernel_size=(3, 3), padding='same')(x)
+        h = BatchNormalization()(h)
+        h = Activation('relu')(h)
+        h = Conv2D(channel_out, kernel_size=(3, 3), padding='same')(h)
+        h = BatchNormalization()(h)
+        h = Add()([h, x])
+        return Activation('relu')(h)
 
 
-model = ResNet34()
-model.build(input_shape=(1, 480, 480, 3))
-model.summary()
+if __name__ == '__main__':
+    mnist = tf.keras.datasets.fashion_mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = (x_train.reshape(-1, 28, 28, 1) / 255).astype(np.float32)
+    x_test = (x_test.reshape(-1, 28, 28, 1) / 255).astype(np.float32)
+    y_train = np.eye(10)[y_train].astype(np.float32)
+    y_test = np.eye(10)[y_test].astype(np.float32)
+
+    resnet = ResNet34((28, 28, 1), 10)
+    model = resnet()
+    model.summary()
+    model.compile(optimizer='adam', 
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=5)
+    model.evaluate(x_test, y_test)
